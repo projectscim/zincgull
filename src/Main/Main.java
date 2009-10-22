e245a955e43e9edabd7ee0ef50d5f784b7ebd37a
herman.rodstrom <herman.rodstrom@c6e2cbe0-bc03-11de-827c-f3460f6bf3f5>
2009-10-22 18:52:19 +0000

    Main goes Mudkipz

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -1,17 +1,83 @@
 package Main;
 
-public class Main {
+import java.applet.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("lol");
-		System.out.println("lulz");
-		System.out.println("heman kan skriva nu!");
-		System.out.println("heman kan skriva igen!");
+import javax.swing.ImageIcon;
+import javax.swing.Timer;
+
+public class Main extends Applet implements ActionListener, KeyListener{
+	private static final long serialVersionUID = 7197415241156375302L;
+	int width, height;
+
+	private ImageIcon sprite=new ImageIcon("images//mudkip.jpg");
+	private Timer tim = new Timer(1,this);
+	private int turned = 1;
+	private int xpos=250;
+	private int ypos=20;
+	private int speed=1;
+	private boolean[] keyDown=new boolean[4];
+	
+	public void init() {
+		width = getSize().width;
+		height = getSize().height;
+		setBackground( Color.black );
+		
+      	this.addKeyListener(this);
+		this.setBackground(Color.BLACK);
+		//this.setDoubleBuffered(true);
+		tim.addActionListener(this);
+		tim.start();
+		this.requestFocus();
 		System.out.println("lulz igen");
+	}
+
+	public void paint( Graphics g ) {
+		super.paint(g);
+		g.drawImage(sprite.getImage(), xpos-turned*250, ypos, turned*500, 500, null);
+		this.requestFocus();
+	}
+
+	public void keyPressed(KeyEvent e) {
+		if(e.getKeyCode()>=37 && e.getKeyCode()<=40)
+			keyDown[40-e.getKeyCode()]=true;
+	}
+
+	
+	public void keyReleased(KeyEvent e) {
+		if(e.getKeyCode()>=37 && e.getKeyCode()<=40)
+			keyDown[40-e.getKeyCode()]=false;
+		
+	}
+
+	public void keyTyped(KeyEvent e) {}
+
+	public void actionPerformed(ActionEvent e) {
+		calculateMove();
+		repaint();
+		
+	}
+
+	private void calculateMove() {
+		if(keyDown[0]){
+			ypos=ypos+speed;
+		}
+		if(keyDown[1]){
+			xpos=xpos+speed;
+			turned = 1;
+		}
+		if(keyDown[2]){
+			ypos=ypos-speed;
+		}
+		if(keyDown[3]){
+			xpos=xpos-speed;
+			turned = -1;
+		}
+		
 	}
 
 }
