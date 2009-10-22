c15900083c5d22c47146f8e2231db92663f80d0f
herman.rodstrom <herman.rodstrom@c6e2cbe0-bc03-11de-827c-f3460f6bf3f5>
2009-10-22 20:01:30 +0000

    försöker få spelytan i en egen klass. ska kalla den frin Zincgull och ha i egen panel

--- trunk/src/client/Zincgull.java
+++ trunk/src/client/Zincgull.java
@@ -1,90 +1,22 @@
 package client;
 
-import java.applet.*;
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
+import java.awt.Graphics;
+import javax.swing.JApplet;
 
-import javax.swing.ImageIcon;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.Timer;
-
-public class Zincgull extends Applet implements ActionListener, KeyListener{
+public class Zincgull extends JApplet{
 	private static final long serialVersionUID = 7197415241156375302L;
-	int width, height;
-
-	private ImageIcon sprite=new ImageIcon("images//zincgull.png");
-	private Timer tim = new Timer(1,this);
-	private int turned = 1;
-	private int xpos = 20;
-	private int ypos = 20;
-	private int speed = 1;
-	private boolean[] keyDown = new boolean[4];
-	
-	private JPanel bottom = new JPanel();
-	private JPanel center = new JPanel();
 	
 	public void init() {
-		width = getSize().width;
-		height = getSize().height;
+		this.setSize(800, 600);
 		
-		bottom.add(new JLabel("bottom"));
-		center.add(new JLabel("center"));
+		new GameArea();
 		
-		this.add(bottom, BorderLayout.SOUTH);
-		this.add(center, BorderLayout.CENTER);
-		
-      	this.addKeyListener(this);
-		tim.addActionListener(this);
-		tim.start();
-		this.requestFocus();
+		this.setVisible(true);
 	}
-
+	
 	public void paint(Graphics g) {
 		super.paint(g);
-		g.drawImage(sprite.getImage(), xpos-turned*28, ypos, turned*76, 56, null);
-		this.requestFocus();
-	}
-
-	public void keyPressed(KeyEvent e) {
-		if(e.getKeyCode()>=37 && e.getKeyCode()<=40)
-			keyDown[40-e.getKeyCode()]=true;
-	}
-
-	
-	public void keyReleased(KeyEvent e) {
-		if(e.getKeyCode()>=37 && e.getKeyCode()<=40)
-			keyDown[40-e.getKeyCode()]=false;
 		
+		new GameArea();
 	}
-
-	public void keyTyped(KeyEvent e) {}
-
-	public void actionPerformed(ActionEvent e) {
-		calculateMove();
-		repaint();
-		
-	}
-
-	private void calculateMove() {
-		if(keyDown[0]){
-			ypos=ypos+speed;
-		}
-		if(keyDown[1]){
-			xpos=xpos+speed;
-			turned = 1;
-		}
-		if(keyDown[2]){
-			ypos=ypos-speed;
-		}
-		if(keyDown[3]){
-			xpos=xpos-speed;
-			turned = -1;
-		}
-		
-	}
-
 }
