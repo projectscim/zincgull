a20f7abc8fb026a2a67fd12a39d657e6cbbadb73
herman.rodstrom <herman.rodstrom@c6e2cbe0-bc03-11de-827c-f3460f6bf3f5>
2009-10-22 22:38:12 +0000

    nu ska appleten fungera i webbläsaren

--- trunk/src/client/GameArea.java
+++ trunk/src/client/GameArea.java
@@ -5,6 +5,7 @@ import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
+import java.net.URL;
 
 import javax.swing.ImageIcon;
 import javax.swing.JPanel;
@@ -13,7 +14,8 @@ import javax.swing.Timer;
 public class GameArea extends JPanel implements ActionListener, KeyListener{
 	private static final long serialVersionUID = -5572295459928673608L;
 	
-	private ImageIcon sprite=new ImageIcon("images//zincgull.png");
+	URL url = getClass().getResource("images/zincgull.png");
+	private ImageIcon sprite = new ImageIcon(url);
 	private Timer tim = new Timer(1,this);
 	private int turned = 1;
 	private int xpos = 20;
