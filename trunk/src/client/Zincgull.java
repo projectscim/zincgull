23428eb033dc221b4b236501dcc4c1bbb7310831
herman.rodstrom <herman.rodstrom@c6e2cbe0-bc03-11de-827c-f3460f6bf3f5>
2009-10-22 21:40:06 +0000

    sidebar har nu en avsluta-knapp, som inte fungerar, men ändå:D

--- trunk/src/client/Zincgull.java
+++ trunk/src/client/Zincgull.java
@@ -1,25 +1,15 @@
 package client;
 
 import java.awt.BorderLayout;
-import java.awt.Color;
 import java.awt.Graphics;
 import javax.swing.JApplet;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
 
 public class Zincgull extends JApplet{
 	private static final long serialVersionUID = 7197415241156375302L;
-	private JPanel game = new GameArea();
-	private JPanel sidebar = new JPanel();
 	
-	public void init() {
-		this.setSize(300, 200);
-		
-		sidebar.add(new JLabel("lol"));
-		sidebar.setBackground(Color.WHITE);
-		
-		this.add(sidebar, BorderLayout.EAST);
-		this.add(game);
+	public void init() {		
+		this.add(new Sidebar(), BorderLayout.EAST);
+		this.add(new GameArea());
 	}
 	
 	public void paint(Graphics g) {
