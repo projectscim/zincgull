66c770dac52ed78a10eb2ebd785b7c269d8ed3b8
andreas.arxe.m <andreas.arxe.m@c6e2cbe0-bc03-11de-827c-f3460f6bf3f5>
2009-10-22 17:25:43 +0000

    Test

--- src/Main/Main.java
+++ src/Main/Main.java
@@ -11,6 +11,7 @@ public class Main {
 		System.out.println("lulz");
 		System.out.println("heman kan skriva nu!");
 		System.out.println("heman kan skriva igen!");
+		System.out.println("lulz igen");
 	}
 
 }
