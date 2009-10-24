a3ef56bb1e92958ec5a1946bba6d560b4fdb5586
herman.rodstrom <herman.rodstrom@c6e2cbe0-bc03-11de-827c-f3460f6bf3f5>
2009-10-24 22:00:17 +0000

    server and client can communicate special commands to each other. next step possibility to change nickname if unique, wont be there in final release, just for testing

--- src/server/ChatSrvThread.java
+++ src/server/ChatSrvThread.java
@@ -19,12 +19,8 @@ public class ChatSrvThread extends Thread {
 			DataInputStream dis = new DataInputStream( socket.getInputStream() );	//gets messages from client
 			while (true) {
 			String message = dis.readUTF();
-			if( message.substring(0, 2).equals("->")){
-				username = message.substring(3);
-				System.out.println( "              username is \""+username+"\"" );
-				server.sendToAll( "-> "+username+" joined, "+ChatSrv.getPeople()+" users online");
-			}else{
-				System.out.println( "MSG "+ChatSrv.getTime()+": Message from "+socket+"\n              "+message );
+			if(!specialCommand(message)){		//check if it's a special command or not
+				System.out.println( "MSG "+ChatSrv.getTime()+": "+message );
 				server.sendToAll( message );
 			}
 			}
@@ -34,5 +30,27 @@ public class ChatSrvThread extends Thread {
 		} finally {
 			server.removeConnection( socket, username );	//closing socket when connection is lost
 		}
+	}
+	
+	void sendTo( String message ) {
+		//synchronized( ChatSrv.enumOutputStreams ) {;		//sync so that no other thread screws this one over
+		try {
+			DataOutputStream dos = new DataOutputStream( socket.getOutputStream() );	//get outputstreams
+			dos.writeUTF( message );		//and send message
+		} catch( IOException ie ) { 
+			System.out.println( ChatSrv.getTime()+": "+ie ); 		//failmsg
+		}
+	}
+	
+	public boolean specialCommand( String msg ){
+		if( msg.substring(0, 5).equals("/nick")){	//expecting a hello-message at first connection
+			username = msg.substring(6);
+			System.out.println( "              username is \""+username+"\"" );
+			server.sendToAll( "-> "+username+" joined, "+ChatSrv.getPeople()+" users online");
+			return true;
+		}else if(msg.substring(0, 6).equals("/users")){
+			sendTo( Integer.toString( ChatSrv.getPeople() ) );
+		}
+		return false;
 	}
 }
