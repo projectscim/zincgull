15706f92f31d9c4f4517be7f82d50748d17ad106
herman.rodstrom <herman.rodstrom@c6e2cbe0-bc03-11de-827c-f3460f6bf3f5>
2009-10-25 16:34:06 +0000

    minor changes in client

--- src/client/Sidebar.java
+++ src/client/Sidebar.java
@@ -1,13 +1,20 @@
 package client;
 
+import java.awt.Dimension;
+
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
 public class Sidebar extends JPanel {
 	private static final long serialVersionUID = 1435646083804109826L;
 	static JLabel lblNick = new JLabel(Zincgull.nick);
+	static JLabel lblHost = new JLabel(Zincgull.host);
+	static JLabel lblRand = new JLabel(Double.toString(Zincgull.random));
 	
 	public Sidebar(){
 		this.add(lblNick);
+		this.add(lblHost);
+		this.add(lblRand);
+		this.setPreferredSize(new Dimension(140, 60));
 	}
 }
