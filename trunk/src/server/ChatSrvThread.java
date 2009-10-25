8911a35d76374f8046a55087699058c83514366d
herman.rodstrom <herman.rodstrom@c6e2cbe0-bc03-11de-827c-f3460f6bf3f5>
2009-10-25 00:06:07 +0000

    made the chat commands failsafe

--- trunk/src/server/ChatSrvThread.java
+++ trunk/src/server/ChatSrvThread.java
@@ -19,9 +19,11 @@ public class ChatSrvThread extends Thread {
 			DataInputStream dis = new DataInputStream( socket.getInputStream() );	//gets messages from client
 			while (true) {
 			String message = dis.readUTF();
-			if(!specialCommand(message)){		//check if it's a special command or not
-				System.out.println( "MSG "+ChatSrv.getTime()+": "+message );
-				server.sendToAll( message );
+			System.out.println( "MSG "+ChatSrv.getTime()+": "+message );
+			
+			if( !specialCommand1(message) ){		//check if it's a special command or not
+				System.out.println( "MSG "+ChatSrv.getTime()+": "+username+": "+message );
+				server.sendToAll( username+": "+message.substring(5) );
 			}
 			}
 		} catch( EOFException ie ) {		//no failmsg
@@ -42,15 +44,103 @@ public class ChatSrvThread extends Thread {
 		}
 	}
 	
-	public boolean specialCommand( String msg ){
-		if( msg.substring(0, 5).equals("/nick")){	//expecting a hello-message at first connection
-			username = msg.substring(6);
-			System.out.println( "              username is \""+username+"\"" );
-			server.sendToAll( "-> "+username+" joined, "+ChatSrv.getPeople()+" users online");
-			return true;
-		}else if(msg.substring(0, 6).equals("/users")){
-			sendTo( Integer.toString( ChatSrv.getPeople() ) );
+	public enum Cmd{	//list available commands
+		msg, nick, hello, users;
+	}
+	
+	public boolean specialCommand3( String msg ){
+		//get the command-part
+		int space = msg.indexOf(" ");
+		String cmd = msg;
+		if(space != -1){
+			cmd = msg.substring(1, space-1);
+		}
+		
+		if( cmd.substring(0, 1).equals("/") ){
+			switch (Cmd.valueOf(cmd)) {
+			case hello:
+				if( msg.length() > 7 ){
+					username = msg.substring(7);
+					System.out.println( "              username is \""+username+"\"" );
+					server.sendToAll( "-> "+username+" joined, "+ChatSrv.getPeople()+" users online");
+					return true;
+				}
+				sendTo( "You need to specify a nickname" );	//colorize this!
+				return false;
+			case users:
+				sendTo( "Currently there are "+Integer.toString( ChatSrv.getPeople() ) +" users online" );
+				return true;
+			case nick:
+				if( msg.length() > 6 ){
+					String tmp = msg.substring(6);
+					System.out.println( "USR "+ChatSrv.getTime()+": "+username+" -> "+tmp );
+					server.sendToAll( username+" became "+tmp );
+					username = tmp;
+					return true;
+				}
+				sendTo( "You need to specify your new nickname" );	//colorize this!
+				return false;
+			case msg:
+				return false;
+			default:
+				sendTo( "Not a real command" );	//colorize this!
+				return false;
+			}
 		}
 		return false;
+	}
+	
+	public boolean specialCommand2( String msg ){
+		if( msg.substring(0, 1).equals("/") && !msg.substring(0, 5).equals("/msg ") ){
+			if( msg.substring(0, 6).equals("/nick ") ){	//expecting a hello-message at first connection
+				String tmp = msg.substring(6);
+				System.out.println( "USR "+ChatSrv.getTime()+": "+username+" -> "+tmp );
+				server.sendToAll( username+" became "+tmp );
+				username = tmp;
+				return true;
+			}else if(msg.substring(0, 6).equals("/users") ){
+				sendTo( "Currently there are "+Integer.toString( ChatSrv.getPeople() ) +" users online" );
+				return true;
+			}else if( msg.substring(0, 7).equals("/hello ") ){	//expecting a hello-message at first connection
+				username = msg.substring(7);
+				System.out.println( "              username is \""+username+"\"" );
+				server.sendToAll( "-> "+username+" joined, "+ChatSrv.getPeople()+" users online");
+				return true;
+			}else{
+				sendTo( "Not a real command" );	//colorize this!
+			}
+			return true;
+		}
+		return false;
+	}
+	
+	public boolean specialCommand1( String msg ){
+		if( msg.substring(0, 1).equals("/") ){
+			if( msg.length() >= 6){
+				if( msg.substring(0, 5).equals("/msg ") ){
+					return false;
+				}else if( msg.substring(0, 6).equals("/nick ") ){	//expecting a hello-message at first connection
+					String tmp = msg.substring(6);
+					System.out.println( "USR "+ChatSrv.getTime()+": "+username+" -> "+tmp );
+					server.sendToAll( username+" became "+tmp );
+					username = tmp;
+					return true;
+				}else if(msg.substring(0, 6).equals("/users") ){
+					sendTo( "Currently there are "+Integer.toString( ChatSrv.getPeople() ) +" users online" );
+					return true;
+				}else if( msg.substring(0, 6).equals("/hello") ){	//expecting a hello-message at first connection
+					if( msg.length() > 7 ){
+						username = msg.substring(7);
+						System.out.println( "              username is \""+username+"\"" );
+						server.sendToAll( "-> "+username+" joined, "+ChatSrv.getPeople()+" users online");
+						return true;
+					}
+					sendTo( "You need a nickname" );	//colorize this!
+					return true;
+				}
+			}
+		}
+		sendTo( "Not a real command" );	//colorize this!
+		return true;
 	}
 }
