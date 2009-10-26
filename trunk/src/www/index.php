ec42c4758c0a524cf1afef79d82a98484a9d1e88
herman.rodstrom <herman.rodstrom@c6e2cbe0-bc03-11de-827c-f3460f6bf3f5>
2009-10-26 02:21:34 +0000

    fixed with webapplet. Make .jar, from /bin: jar -cvf ..\src\Zincgull.jar client\*.class images\Zincgull.png Sign jarfile, from /src/www: jarsigner Zincgull.jar key Type password: asdfgh

--- trunk/src/www/index.php
+++ trunk/src/www/index.php
@@ -3,7 +3,7 @@
 <title>Zincgull MMORPG</title>
 </head>
 <body>
-<applet code=client.Zincgull.class width=100% height=100% >
+<applet archive="Zincgull.jar" code=client.Zincgull.class width=100% height=100% >
 	<param name="host" value="server.rodstrom.se">
 	<param name="nick" value="web"> 
 </applet> 
