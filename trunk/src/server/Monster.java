0f0d7eee480403c9639ed3f3aca3eb539fcaad77
andreas.arxe.m <andreas.arxe.m@c6e2cbe0-bc03-11de-827c-f3460f6bf3f5>
2010-01-24 20:36:51 +0000

    - Added a very basic monster class (Monster), atm it's only variables and getters & setters. - Added a class called "MonsterDatabase", atm it is able to load monsters from file and print out stats of a random monster to console.

---
+++ trunk/src/server/Monster.java
@@ -0,0 +1,88 @@
+package server;
+
+/**
+ * 
+ * 
+ * 
+ * @author Andreas
+ */
+public class Monster implements Runnable {
+	
+	private String name;
+	private int damage;
+	private int health;
+	private int level;
+	private int aggro;
+	private String spawnLocation;
+	private boolean boss;
+
+	
+	public Monster() {
+		
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getDamage() {
+		return damage;
+	}
+
+	public void setDamage(int damage) {
+		this.damage = damage;
+	}
+
+	public int getHealth() {
+		return health;
+	}
+
+	public void setHealth(int health) {
+		this.health = health;
+	}
+
+	public int getLevel() {
+		return level;
+	}
+
+	public void setLevel(int level) {
+		this.level = level;
+	}
+
+	public int getAggro() {
+		return aggro;
+	}
+
+	public void setAggro(int aggro) {
+		this.aggro = aggro;
+	}
+
+	public String getSpawnLocation() {
+		return spawnLocation;
+	}
+
+	public void setSpawnLocation(String spawnLocation) {
+		this.spawnLocation = spawnLocation;
+	}
+
+	public boolean isBoss() {
+		return boss;
+	}
+
+	public void setBoss(boolean boss) {
+		this.boss = boss;
+	}
+
+	public void run() {
+
+		while(true) {
+			//TODO Stuff
+		}
+		
+	}
+	
+}
