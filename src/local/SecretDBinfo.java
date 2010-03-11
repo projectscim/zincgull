8d8a8a1d96382ecc7156dd97e7cb28f3f50cc5fd
andreas.arxe.m <andreas.arxe.m@c6e2cbe0-bc03-11de-827c-f3460f6bf3f5>
2010-03-11 15:36:51 +0000

    - SecretDBinfo-skel, used by MonsterDatabase.

---
+++ src/local/SecretDBinfo.java
@@ -0,0 +1,9 @@
+package local;
+
+public interface SecretDBinfo {
+	static final String host = "******";
+	static final String database = "******";
+	static final String user = "*****";
+	static final String pass = "*****";
+	static final String url = "jdbc:mysql://"+host+"/"+database;
+}
